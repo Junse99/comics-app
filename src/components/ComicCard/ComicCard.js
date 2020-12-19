bf835e6e906e52b2908f5a11f3f82ceb5e8181c4
Sebastian Obregón <sebasobregon99@hotmail.com>
2020-12-19 18:31:00 -0500

    up CRUD

--- src/components/ComicCard/ComicCard.js
+++ src/components/ComicCard/ComicCard.js
@@ -1,86 +1,114 @@
-import {React, useState} from 'react';
-import {Button} from 'antd';
-import './ComicCard.css';
-import AdminModal from '../AdminModal/AdminModal'
+import { Fragment, React, useState } from "react";
+import { Button } from "antd";
+import "./ComicCard.css";
+import AdminModal from "../AdminModal/AdminModal";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
+import { comicActions } from "../../redux/services/comics/comicSlice";
 
-const ComicCard = ({comic, onSelect}) => {
+const ComicCard = ({ comic, onSelect }) => {
+  const [visible, setVisible] = useState(false);
+  const { title, id, description, state, thumbnail } = comic;
 
-    const [visible, setVisible] = useState(false);
-    const {
-        title, 
-        id, 
-        description,
-        state,
-        thumbnail} = comic;
+  const dispatch = useDispatch();
 
-    const onClick = () => {
-        if(onSelect) {
-            onSelect(comic)
-        }
-    };
+  const {comics} = useSelector(state => state.comics)
 
-    const abrirModal = () => {
-        setVisible(true);
-      };
-
-    let message = 'A revisar';
-    let buttonType = 'primary';
-    switch(comic.state){
-        case 'NEW': 
-            buttonType = 'default';
-            message = 'Añadir a revisados';
-            break;
-        case 'REVIEW': 
-            buttonType = 'primary';
-            message = 'Añadir a la lista';
-            break;
-        case 'APPROVED':
-            buttonType = 'text';
-            message = 'Completado';
-            break;
-        default:
-            buttonType = 'text';
-            message = 'Sin estado';
-            break;
+  const onClick = () => {
+    if (onSelect) {
+      onSelect(comic);
     }
+  };
 
-    return (
-        <div className="card-comic-container">
-            <div className="img-card">
-                <img src={(thumbnail ? `${thumbnail.path}.${thumbnail.extension}` : null) || "https://cdn.domestika.org/c_limit,dpr_auto,f_auto,q_auto,w_820/v1471610692/content-items/001/689/946/potada_comic-original.jpg?1471610692"}></img>
-            </div>
-            <div className="right-content-card">
-                <div className="title-card">
-                    <label><b>Titulo: </b>{title}</label>
-                </div>
-                <div className="description-card">
-                    <p>{description}</p>
-                </div>
-            </div>
-            <div>
-            {
-                onSelect && (<Button
-                    type={buttonType}
-                    style={{ marginBottom: "10px" }}
-                    onClick={onClick}
-                  >
-                      {message}
-              </Button>)
-            }
-            {
-                state === "REVIEW" && (
-                <Button
-                    type="danger"
-                    style={{ marginBottom: "10px", marginLeft: "10px"}}
-                    onClick={abrirModal}
-                  >
-                      Editar
-              </Button>)
-            }
-            </div>
-            <AdminModal comic = {comic} visible={visible} setVisible={setVisible} title="Editar Comic"></AdminModal>
+  const abrirModal = () => {
+    setVisible(true);
+  };
+
+  const deleteComic = ()=> {
+      dispatch(comicActions.setComics({
+          ...comics,
+          reviewComics: comics.reviewComics.filter(i => i.id !== comic.id)
+      }))
+  };
+
+  let message = "A revisar";
+  let buttonType = "primary";
+  switch (comic.state) {
+    case "NEW":
+      buttonType = "default";
+      message = "Añadir a revisados";
+      break;
+    case "REVIEW":
+      buttonType = "primary";
+      message = "Añadir a la lista";
+      break;
+    case "APPROVED":
+      buttonType = "text";
+      message = "Completado";
+      break;
+    default:
+      buttonType = "text";
+      message = "Sin estado";
+      break;
+  }
+
+  return (
+    <div className="card-comic-container">
+      <div className="img-card">
+        <img
+          src={
+            (thumbnail ? `${thumbnail.path}.${thumbnail.extension}` : null) ||
+            "https://cdn.domestika.org/c_limit,dpr_auto,f_auto,q_auto,w_820/v1471610692/content-items/001/689/946/potada_comic-original.jpg?1471610692"
+          }
+        ></img>
+      </div>
+      <div className="right-content-card">
+        <div className="title-card">
+          <label>
+            <b>Titulo: </b>
+            {title}
+          </label>
         </div>
-    );
-}
+        <div className="description-card">
+          <p>{description}</p>
+        </div>
+      </div>
+      <div>
+        {onSelect && (
+          <Button
+            type={buttonType}
+            style={{ marginBottom: "10px" }}
+            onClick={onClick}
+          >
+            {message}
+          </Button>
+        )}
+        {state === "REVIEW" && (
+          <Fragment>
+            <Button success
+
+              style={{ marginBottom: "10px", marginLeft: "10px" }}
+              onClick={abrirModal}
+            >
+              Editar
+            </Button>
+            <Button
+              type="danger"
+              style={{ marginBottom: "10px", marginLeft: "10px" }}
+              onClick={deleteComic}
+            >
+              Eliminar
+            </Button>
+          </Fragment>
+        )}
+      </div>
+      <AdminModal
+        comic={comic}
+        visible={visible}
+        setVisible={setVisible}
+        title="Editar Comic"
+      ></AdminModal>
+    </div>
+  );
+};
 
 export default ComicCard;
